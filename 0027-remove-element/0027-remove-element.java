50015238ffc6b41a963038063cc22c3e15f55674
AMrr <55325468+AmmrFX@users.noreply.github.com>
2022-10-23 11:39:18 +0200

    Time: 1 ms (48.10%), Space: 42.3 MB (72.51%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count =0;
+        for(int i = 0 ;i<nums.length;i++){
+           if(nums[i]==val){
+            count++;
+        }else
+            nums[i-count] = nums[i];
+        }
+        return nums.length- count;
+    }
+}
